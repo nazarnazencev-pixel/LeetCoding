eacff9f0bf9dda3822329e4bd526841875f6be86
nazarnazencev-pixel <nazarnazencev@gmail.com>
2026-07-20 11:48:42 +0300

    Create Task_83.java

---
+++ Easy/Task_83.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = head;
+
+        while (res != null && res.next != null) {
+            if (res.val == res.next.val) {
+                res.next = res.next.next;
+            } else {
+                res = res.next;
+            }
+        }
+
+        return head;        
+    }
+}
